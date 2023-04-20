8191f83baeaac5ec5a5149264b19d042bbd82e34
Victor Cunillera <victor2003cn@gmail.com>
2023-04-20 16:37:42 +0200

    Changes

--- WarOfKingdoms/app/src/main/java/com/example/warofkingdoms/Login.java
+++ WarOfKingdoms/app/src/main/java/com/example/warofkingdoms/Login.java
@@ -2,7 +2,9 @@ package com.example.warofkingdoms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -12,3 +14,13 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 }
+
+    /*
+    public void onClick(View view) {
+        Intent intent = new Intent(activity_login.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+}
+*/
