f27583d6fd38f4e998f3f0a7c29e7f2d9b26604d
Victor Cunillera <victor2003cn@gmail.com>
2023-05-14 18:27:12 +0200

    Turn

--- WarOfKingdoms/app/src/main/java/com/example/warofkingdoms/Turn.java
+++ WarOfKingdoms/app/src/main/java/com/example/warofkingdoms/Turn.java
@@ -1,14 +1,52 @@
 package com.example.warofkingdoms;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Turn {
     private Player activePlayer;
     private Player inactivePlayer;
     private int turnNumber;
+    private Timer timer;
+    private int timerDuration;
 
     public Turn(Player activePlayer, Player inactivePlayer, int turnNumber) {
         this.activePlayer = activePlayer;
         this.inactivePlayer = inactivePlayer;
         this.turnNumber = turnNumber;
+        this.timer = timer;
+    }
+
+    public Turn(int timerDuration) {
+        this.timerDuration = timerDuration;
+    }
+
+    public void iniciar() {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // When it runs to 0
+                System.out.println("¡El tiempo del turno ha terminado!");
+            }
+        }, timerDuration * 1000);
+    }
+    public void detener() {
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
+    public static void main(String[] args) {
+        Turn turn = new Turn(40); // Turn duration: 40 seconds
+        turn.iniciar();
+
+        // Waits 5 seconds and stops de counter
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        turn.detener();
     }
 
     public void playCard(Card card, int position) {
